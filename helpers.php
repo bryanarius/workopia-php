b51005ba01ae3c73cbc84902c22b5340f0d9d978
Bryan Arius <bryanarius@gmail.com>
2024-02-11 22:55:26 -0500

    Split UI into partials

--- helpers.php
+++ helpers.php
@@ -10,3 +10,39 @@
 function basePath($path = '') {
     return __DIR__ . '/' . $path;
 }
+
+/** 
+ * Get the base path
+ * 
+ * @param string $name
+ * @return void
+*/
+
+function loadView ($name) {
+
+    $viewPath =  basePath("views/${name}.view.php");
+
+    if(file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+/** 
+ * Get the base path
+ * 
+ * @param string $name
+ * @return void
+*/
+
+function loadPartial ($name) {
+    $partialPath = basePath("views/partials/${name}.php");
+
+    if(file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name} not found!'";
+    }
+    
+}
